7654b748e3e6a3064bd9f712868d78af0c04c0e1
duff2013 <cmduffy@engr.psu.edu>
2014-11-24 01:37:15 -0500

    new example

---
+++ examples/deepsleep/deepSleep_all_wakeups/deepSleep_all_wakeups.ino
@@ -0,0 +1,99 @@
+/***************************************
+ * this shows all the wakeups
+ * for deepSleep & hibernate. 
+ * expect IDD of around 230uA.
+ ****************************************/
+#include <Snooze.h>
+
+SnoozeBlock config;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  /****************************
+   * define the configuration 
+   * parameters for waking the
+   * teensy up.
+   *****************************/
+  config.pinMode(21, INPUT_PULLUP, RISING);//pin, mode, type
+  config.pinMode(22, INPUT_PULLUP, RISING);//pin, mode, type
+  //config.setTimer(1000);// milliseconds
+  config.setAlarm(0, 0, 10);// hour, min, sec
+  config.pinMode(12, CMP, RISING, 1.123);//pin, mode, type, threshold
+  config.pinMode(0, TSI, touchRead(0)+250);//pin, mode, threshold
+}
+
+
+void loop() {
+  /************************
+   * feed the sleep function
+   * its wakeup parameters.
+   *************************/
+  Snooze.deepSleep( config );
+  int who = Snooze.source( );// get wakeup source 
+  
+  if(who == 21) {// pin wakeup source is its pin value
+    for(int i = 0; i < 1; i++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(200);
+    }
+  }
+
+  if(who == 22) {// pin wakeup source is its pin value
+    for(int i = 0; i < 2; i++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(200);
+    }
+  }
+
+  if(who == 34) { // compare wakeup value
+    for(int i = 0; i < 3; i++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(200);
+    }
+  }
+
+  if(who == 35) {// rtc wakeup value
+    for(int i = 0; i < 4; i++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(200);
+    }
+  }
+
+  if(who == 36) {// lptmr wakeup value
+    for(int i = 0; i < 5; i++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(200);
+    }
+  }
+
+  if(who == 37) {// tsi wakeup value
+    for(int i = 0; i < 6; i++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(200);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
